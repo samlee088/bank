9e7a3c498091e30def51fc97439b67330ede0c13
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-30 19:05:38 -0700

    transaction classes

--- main.py
+++ main.py
@@ -16,3 +16,27 @@ class Transaction(ABC):
     @abstractmethod
     def get_transaction_description(self):
         pass
+
+
+class Deposit(Transaction):
+    def __init__(self, customerId, tellerId, amount):
+        super().__init__(customerId, tellerId)
+        self.amount = amount
+    
+    def get_transaction_description(self):
+        return f'Teller {self.get_teller_id()} deposited {self._amount} to account {self.get_customer_id()}' 
+
+class Withdrawal(Transaction):
+    def __init__(self, customerId, tellerId, amount):
+        super().__init__(customerId, tellerId)
+        self.amount = amount
+
+    def get_transaction_description(self):
+        return f'Teller {self.get_teller_id()} withdrew {self._amount} from account {self.get_customer_id()}'
+    
+class OpenAccount(Transaction):
+    def __init__(self, customerId, tellerId):
+        super().__init__(customerId, tellerId)
+
+    def get_transaction_description(self):
+        return f'Teller {self.get_teller_id()} opened account {self.get_customer_id()}'
